8a00ecf07ecc1ef79570942a208a0fae6246abe1
Gradus Smid <33928046+GradusSmid@users.noreply.github.com>
2021-12-22 10:35:38 +0100

    Added simple color change

--- Assets/Scripts/ColorChange.cs
+++ Assets/Scripts/ColorChange.cs
@@ -6,6 +6,14 @@ public class ColorChange : MonoBehaviour
 {   
     void Update()
     {
-        transform.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+       MeshRenderer[] mad = transform.GetComponentsInChildren<MeshRenderer>();
+
+        for (int i = 0; i < mad.Length; i++)
+        {
+            if(mad[i].material.name == "Outside (Instance)")
+            {
+                mad[i].material.SetColor("_Color", Color.red);
+            }
+        }
     }
 }
